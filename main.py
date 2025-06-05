6d217ed27ac474d7d76e078fd3e1005b2bf39dba
roque121007 <jesusalexanderroquetrejo90@gmail.com>
2025-06-05 16:28:02 -0600

    Renombrar funciones para mejorar la claridad: 'read_root' a 'root' y 'obtener_tutores' a 'leer_tutores'.

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import pandas as pd
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def root():
     return {"mensaje": "API activa"}
 
 @app.get("/tutores")
-def obtener_tutores():
+def leer_tutores():
     try:
-        df = pd.read_excel("Base_de_datos_tutor.xlsx")  # Asegúrate de que el nombre del archivo sea correcto y esté en el mismo directorio
+        df = pd.read_excel("Base_de_datos_tutor.xlsx")  # Asegúrate que este archivo esté en el root
         return df.to_dict(orient="records")
     except Exception as e:
         return {"error": str(e)}
